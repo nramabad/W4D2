b519008e1dd2fe9c15d9486c33203929d0b9ae6b
nramabad <nramabad@gmail.com>
2018-11-13 17:59:06 -0800

    Make partial to autofill :new and :edit views with similar content

--- ninety_nine_cats/app/controllers/cats_controller.rb
+++ ninety_nine_cats/app/controllers/cats_controller.rb
@@ -11,9 +11,33 @@ class CatsController < ApplicationController
   end
 
   def new
+    @cat = Cat.new
     render :new
   end
 
+  def edit
+    @cat = Cat.find(params[:id])
+    render :edit
+  end
 
+  def create
+    cat = Cat.new(cats_params)
+
+    redirect_to (cat.save ? cats_url : new_cat_url)
+  end
+
+  def update
+    cat = Cat.find(params[:id])
+    redirect_to (cat.update(cats_params) ? cat_url(cat) : edit_cat_url(cat))
+  end
+
+  def persisted?
+    Cat.find_by(id: params[:id])
+  end
+
+  private
+  def cats_params
+    params.require(:cats).permit(:name, :sex, :color, :description, :birth_date)
+  end
 
 end
